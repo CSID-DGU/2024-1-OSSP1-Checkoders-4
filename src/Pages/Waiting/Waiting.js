ab9dcd7587369fbcdd7bf502b34a61d67ee65adf
Yunsu0407 <aszx4949@naver.com>
2024-05-24 00:00:25 +0900

    json complete

--- src/Pages/Waiting/Waiting.js
+++ src/Pages/Waiting/Waiting.js
@@ -1,16 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import response_ex from "./login_dummy_data.json";
+import data from "./data.json";
+import data2 from "./data2.json"
 
 const API_BASE_URL = process.env.REACT_APP_LOCAL_API_BASE_URL;
 
 function Waiting() {
   const navigate = useNavigate();
-  const code = new URL(window.location.href).searchParams.get('code');
+  const code = new URL(window.location.href).searchParams.get('code');  // code 가져오는 문장
 
   const handleData = (response_id, response_nickname) => {
-    navigate('/Practice', { state: { id: response_id, nickname: response_nickname } });
+    navigate('/Main', { state: { id: response_id, nickname: response_nickname } });
   };
 
   useEffect(() => {
@@ -21,24 +22,23 @@ function Waiting() {
         .then((response) => {
           // 요청 성공 시 실행되는 코드
           console.log('kakaoLogin success');
-          handleData(response.data.id, response.data.nickname);
+          // handleData(response.data.id, response.data.nickname);
         })
         .catch(error => {
           // 요청 실패 시 실행되는 코드
           console.log('kakaoLogin fail');
-          // changeTmp(response_ex.id);
-          // handleData(response_ex.id, response_ex.nickname);
+          // handleData(data.myData[0].id, data.myData[0].nickname);
         });
     };
 
     fetchData();
-  }, [code]);
+  });
 
   return (
     <div>
-      로그인을 기다리는 페이지입니다.
-      {response_ex.id}
-      공백이 생기나
+      로그인을 기다리는 페이지입니다.<br/>
+      {data.myData[0].id}<br/>
+      {data.myData[0].nickname}<br/>
     </div>
   );
 }
--- src/components/MainPage/MainPage2.js
+++ src/components/MainPage/MainPage2.js
@@ -2,7 +2,7 @@ import './Foundation.css';
 import './MainPage2.css';
 import './CalendarStyle.css';
 import { AiOutlineHome } from "react-icons/ai";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import logo from '../동국대로고.png';
 import TaskCalendar from './TaskCalendar.js';
@@ -38,8 +38,8 @@ function MainPage2(props) {
           </button>
         </div>
       </div>
-      <div className='bottomBox' style={{flexDirection: 'column'}}>
-        
+      <div className='bottomBox' style={{ flexDirection: 'column' }}>
+
         <div className='Main-name'>
           <AiOutlineHome className="home-icon" />
           메인페이지
@@ -48,38 +48,38 @@ function MainPage2(props) {
         </div>
 
         <div className="main-bottom-box">
-            <div className="main-container">
+          <div className="main-container">
 
-               <div className="main-container-title">
-                  현재 진행중인 클래스
-                  {/* <PopUp /> */}
-               </div> 
-               <div className="main-container-box">
-                  <ClassComponent lecture_name={lecture_name}/>
-
-                  <ClassComponent lecture_name={lecture_name}/>
-
-                  <ClassComponent lecture_name={lecture_name}/>
-
-                  <ClassComponent lecture_name={lecture_name}/>
-               </div>
-
+            <div className="main-container-title">
+              현재 진행중인 클래스
+              {/* <PopUp /> */}
             </div>
-            <div className="name-calendar-container">
-                <div className="name-container">
-                    <div className="name-logo-container">
-                      이름: {nickname}
-                      <img src={logo} alt="동국대로고" style={{ width: '6vw', height: 'auto'}} />
-                    </div>
-                </div>
+            <div className="main-container-box">
+              <ClassComponent lecture_name={lecture_name} />
 
-                <div className= "main-task-calendar">
-                   <TaskCalendar className="react-calendar"/>
-                </div>
-                <div className= "main-task-info">
-                   <TaskInfo lecture_name={lecture_name}/>
-                </div>
+              <ClassComponent lecture_name={lecture_name} />
+
+              <ClassComponent lecture_name={lecture_name} />
+
+              <ClassComponent lecture_name={lecture_name} />
             </div>
+
+          </div>
+          <div className="name-calendar-container">
+            <div className="name-container">
+              <div className="name-logo-container">
+                이름: {nickname}
+                <img src={logo} alt="동국대로고" style={{ width: '6vw', height: 'auto' }} />
+              </div>
+            </div>
+
+            <div className="main-task-calendar">
+              <TaskCalendar className="react-calendar" />
+            </div>
+            <div className="main-task-info">
+              <TaskInfo lecture_name={lecture_name} />
+            </div>
+          </div>
         </div>
 
       </div>
