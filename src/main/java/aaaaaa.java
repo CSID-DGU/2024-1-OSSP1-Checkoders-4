cb75e3382756f670821bacb2f2f3081d6d00e593
sunny <122611609+sunnyanna0@users.noreply.github.com>
2024-06-16 22:21:19 +0900

    Add test.txt file

---
+++ src/main/java/aaaaaa.java
@@ -0,0 +1,47 @@
+/*
+public class Hello {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            for (String arg : args) {
+                System.out.print(arg+"hihi");
+            }
+        } else {
+            System.out.println("인자가 전달되지 않았습니다.");
+        }
+    }
+}
+*/
+
+/*
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
+*/
+/*
+public class Hello {
+    public static void main(String[] args) {
+        String input = args[0];
+        String output = "";
+
+        if (input.equals("test1")) {
+            output = "answer1";
+        } else if (input.equals("test2")) {
+            output = "answer2";
+        } else if (input.equals("test3")) {
+            output = "answer3";
+        } else if (input.equals("test4")) {
+            output = "answer4";
+        } else if (input.equals("test5")) {
+            output = "answer5";
+        } else {
+            output = "Invalid test input";
+        }
+        System.out.println(output);
+    }
+}
+
+*/
+
+
---
+++ src/main/java/zxcv/asdf/repository/AnswerRepository.java
@@ -0,0 +1,12 @@
+package zxcv.asdf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zxcv.asdf.domain.Answer;
+import zxcv.asdf.domain.LectureAssignment;
+import zxcv.asdf.domain.User;
+
+import java.util.Optional;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    Optional<Answer> findByUserAndAssignment(User user, LectureAssignment assignment);
+}
